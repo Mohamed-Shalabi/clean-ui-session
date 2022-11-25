8526a527ed6f7094e54ba6ea0c16abe327eb6448
Mohamed-Shalabi <mshalaby10@gmail.com>
2022-11-25 18:50:18 +0200

    Removed useless code

--- lib/styles/app_fonts.dart
+++ lib/styles/app_fonts.dart
@@ -11,12 +11,3 @@ abstract class AppFontWeights {
   static const regular = FontWeight.w400;
   static const light = FontWeight.w300;
 }
-
-abstract class AppFontSizes {
-  static const s12 = 12.0;
-  static const s14 = 14.0;
-  static const s16 = 16.0;
-  static const s18 = 18.0;
-  static const s20 = 20.0;
-  static const s22 = 22.0;
-}
