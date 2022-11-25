be20ff6534406a1e3ab8ad3a5ac4c2613e764b1a
Mohamed-Shalabi <mshalaby10@gmail.com>
2022-11-25 18:55:16 +0200

    Removed useless code

--- lib/styles/app_text_styles.dart
+++ lib/styles/app_text_styles.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
-
-import 'app_fonts.dart';
-
-abstract class AppTextStyles {
-  static const appBarTitleStyle = TextStyle(
-    fontSize: AppFontSizes.s16,
-    fontWeight: AppFontWeights.medium,
-  );
-}
+abstract class AppTextStyles {}
